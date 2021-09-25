e0efb7abd7de44a53d414dc71b7d0152799f883b
Lukas Möller <lm.lukasmoeller@googlemail.com>
2021-09-25 21:03:20 +0200

    until section 317

--- controllers/admin.js
+++ controllers/admin.js
@@ -53,7 +53,9 @@ exports.postAddProduct = (req, res, next) => {
 			res.redirect('/admin/products');
 		})
 		.catch((err) => {
-			console.log(err);
+			const error = new Error(err);
+			error.httpStatusCode = 500;
+			return next(error);
 		});
 };
 
@@ -78,7 +80,11 @@ exports.getEditProduct = (req, res, next) => {
 				validationErrors: [],
 			});
 		})
-		.catch((err) => console.log(err));
+		.catch((err) => {
+			const error = new Error(err);
+			error.httpStatusCode = 500;
+			return next(error);
+		});
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -121,8 +127,11 @@ exports.postEditProduct = (req, res, next) => {
 				res.redirect('/admin/products');
 			});
 		})
-
-		.catch((err) => console.log(err));
+		.catch((err) => {
+			const error = new Error(err);
+			error.httpStatusCode = 500;
+			return next(error);
+		});
 };
 
 exports.getProducts = (req, res, next) => {
@@ -137,7 +146,11 @@ exports.getProducts = (req, res, next) => {
 				path: '/admin/products',
 			});
 		})
-		.catch((err) => console.log(err));
+		.catch((err) => {
+			const error = new Error(err);
+			error.httpStatusCode = 500;
+			return next(error);
+		});
 };
 
 exports.postDeleteProduct = (req, res, next) => {
@@ -147,5 +160,9 @@ exports.postDeleteProduct = (req, res, next) => {
 			console.log('DESTROYED PRODUCT');
 			res.redirect('/admin/products');
 		})
-		.catch((err) => console.log(err));
+		.catch((err) => {
+			const error = new Error(err);
+			error.httpStatusCode = 500;
+			return next(error);
+		});
 };
